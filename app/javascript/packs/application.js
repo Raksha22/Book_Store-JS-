a569c3ea98398e3394421d52333e1addf4bf9614
rails <raksha.ramteke22@gmail.com>
2021-02-02 16:21:58 +0530

    edit application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,14 +2,23 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+//= require rails-ujs
+//= require activestorage
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+//= require_self
+//= require_tree
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
